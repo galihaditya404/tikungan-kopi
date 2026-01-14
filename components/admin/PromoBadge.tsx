6f7313d7dc96bb1bc206f30fcda40aee39004cb0
Galih Aditya <ghalih404@gmail.com>
2026-01-14 21:40:24 +0700

    fix promo

--- components/admin/PromoBadge.tsx
+++ components/admin/PromoBadge.tsx
@@ -2,7 +2,7 @@ export default function PromoBadge({ isPromo }: { isPromo?: boolean }) {
   if (!isPromo) return null;
   return (
     <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800">
-      Promo
+      Promo's
     </span>
   );
 }
